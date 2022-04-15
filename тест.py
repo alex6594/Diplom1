a211dc4afc1b1280d0daf50d53e9f8a91717c3cb
Алексей Кувшинов <alex6594@mail.ru>
2022-04-15 13:36:09 +0300

    Fixed

---
+++ тест.py
@@ -0,0 +1,3 @@
+x=[{"file_name": "17_1562944607.jpg", "size": "x"}, {"file_name":"11_1593979270.jpg", "size": "y"}, {"file_name": "6_1594036409.jpg", "size": "y"}, {"file_name": "9_1594036429.jpg", "size": "y"}, {"file_name": "9_1594036496.jpg", "size": "z"}]
+for files in range(len(x)):
+ print(x[files]['file_name'])
